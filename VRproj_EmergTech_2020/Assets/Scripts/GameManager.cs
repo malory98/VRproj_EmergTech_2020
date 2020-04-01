d8f1226dc47a3f4473a01957d72ed4c03da71ba2
Lyssa <lyssa.tino@gmail.com>
2020-03-31 20:34:13 -0400

    Added GameManager
    basic health system in place

---
+++ VRproj_EmergTech_2020/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int lives = 3;
+    public Transform spawnpoint;
+    public GameObject player;
+
+
+    public void LoseLife()
+    {
+        lives--;
+        CheckLives();
+    }
+
+    public void CheckLives()
+    {
+        if (lives == 0)
+        {
+            ZeroLives();
+        }
+    }
+
+    public void ZeroLives()
+    {
+        //restart from beginning
+        player.transform.position = spawnpoint.position;
+        lives = 3;
+    }
+}
