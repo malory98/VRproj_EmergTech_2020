d826266ba599e79aace768253aec83d0de093222
Lyssa <lyssa.tino@gmail.com>
2020-04-03 15:24:58 -0400

    Added Scene script / Started stage 3

---
+++ VRproj_EmergTech_2020/Assets/Scripts/SceneChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*******************************
+ Name: Lyssa Tino
+ Course: Emerging Technologies
+ Project: Wake Up
+*******************************/
+
+public class SceneChange : MonoBehaviour
+{
+    public string sceneName;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
